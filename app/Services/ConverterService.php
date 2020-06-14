443433285940edce288c2990518909a0bfa12293
rkasatin <kasatkin@azoft.com>
2020-06-14 23:28:00 +0700

    Second source

--- app/Services/ConverterService.php
+++ app/Services/ConverterService.php
@@ -39,4 +39,45 @@ class ConverterService
         }
         return $exchangeRates;
     }
+
+    /**
+     * @return array
+     */
+    public function getExchangeRates2(){
+        $exchangeRates = Cache::get('exchange_rates', []);
+        if (!count($exchangeRates)) {
+            try {
+                $url = 'https://api.exchangeratesapi.io/latest?symbols=RUB&';
+                $currencyList = config('currencies.currencies');
+
+                foreach ($currencyList as $cl) {
+                    $curl = curl_init();
+                    curl_setopt_array($curl, [
+                        CURLOPT_URL => $url . "&base=" . $cl,
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => "",
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => "GET",
+                    ]);
+
+                    $response = curl_exec($curl);
+                    if ($response) {
+                        $response = json_decode($response);
+                    }
+
+                    $exchangeRates[$cl] = $response->rates->RUB;
+                    curl_close($curl);
+                }
+
+                Cache::set('exchange_rates', $exchangeRates, config('currencies.cache_timeout'));
+            } catch (\Exception $e) { // на всякий случай обработчик ошибок
+                echo 'Error: ' . $e->getMessage();
+            }
+
+        }
+        return $exchangeRates;
+    }
 }
